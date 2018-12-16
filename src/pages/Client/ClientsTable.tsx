ae71136a5d9cb405931497bdf3378bb79aa91c32
Freddy ROBERGE <freddy.roberge@gmail.com>
2018-12-16 22:46:43 +0100

    Create/Delete client

--- src/pages/Client/ClientsTable.tsx
+++ src/pages/Client/ClientsTable.tsx
@@ -9,17 +9,32 @@ import 'react-table/react-table.css';
 import { IClient } from 'src/interfaces/IClient';
 import { observer } from 'mobx-react';
 import { toJS, observable } from 'mobx';
-import { Icon, Dialog, Button, Intent } from '@blueprintjs/core';
+import { Icon, Dialog, Button, Intent, InputGroup } from '@blueprintjs/core';
 import { NewElementButton } from 'src/components/NewElementButton';
 
 interface IProps {
     client: IClient;
 //    handler: (client: IClient) => void;
 }
+
+const dialogLineStyle = style(csstips.margin(10), csstips.flex, csstips.horizontal);
+const dialogFieldNameStyle = style(csstips.width(80), csstips.margin(5, 5));
+const dialogFieldValueStyle = style(csstips.flex);
+
 @observer export class ClientsTable extends React.Component<IProps, {}> {
 
-    @observable clients: IClient[] = [];
-    @observable clientToDelete: IClient | undefined = undefined
+    @observable private clients: IClient[] = [];
+    @observable private clientToDelete: IClient | undefined = undefined;
+    @observable private dialogCreateClientOpened: boolean = false;
+    private password: string = '';
+    private clientToCreate: IClient = {
+        id: undefined,
+        nom: undefined,
+        adresse: undefined,
+        telephone: undefined,
+        email: undefined
+    };
+
     // https://react-table.js.org/#/story/readme
     constructor(props: IProps) {
         super(props);
@@ -43,6 +58,7 @@ interface IProps {
     onRowClick = (state: any, rowInfo: any, column: any, instance: any) => {
         return {
             onClick: (e: any) => {
+                console.log(rowInfo.original)
             //   var client = rowInfo.original;
             // this.props.handler( client );
             // console.log('A Td Element was clicked!')
@@ -94,13 +110,6 @@ interface IProps {
 
         ];
 
-        // if ( this.clients.length === 0 || ( this.clients.length === 1 && this.clients[0] === undefined ) ) {
-        // if ( this.props.client === undefined ) {
-        //     return (
-        //         <div/>
-        //     );
-        // }
-
         return (
 
             <div>
@@ -120,47 +129,150 @@ interface IProps {
                 <NewElementButton
                     name="Create new Client"
                     onClick={() => { 
-                        let client: IClient = {
-                            id: undefined,
-                            nom: 'essai',
-                            adresse: 'adresse essai',
-                            telephone: 'tel essai',
-                            email: 'a@a.a'
-                        };
-                        this.handleWriteClient(client);
+                        this.dialogCreateClientOpened = true;
+                        // let client: IClient = {
+                        //     id: undefined,
+                        //     nom: 'essai',
+                        //     adresse: 'adresse essai',
+                        //     telephone: 'tel essai',
+                        //     email: 'a@a.a'
+                        // };
+                        // this.handleWriteClient(client);
                     }}
                 />
-                {
-                    this.clientToDelete ?
-                    <Dialog
-                        autoFocus={true}
-                        enforceFocus={true}
-                        usePortal={true}
-                        canOutsideClickClose={true}
-                        canEscapeKeyClose={true}
-                        isOpen={this.clientToDelete !== undefined}
-                        title={'Suppression client "' + this.clientToDelete.nom + '", id[' + this.clientToDelete.id + ']'}
-                        icon="warning-sign"
-                        onClose={() => { this.clientToDelete = undefined; }}
-                    >
-                        <div className={style(csstips.flex, csstips.vertical)}>
-                            <div className={style(csstips.margin(20), csstips.flex)}>
-                                Opération irréversible : confirmer ?
+                <Dialog
+                    autoFocus={true}
+                    enforceFocus={true}
+                    usePortal={true}
+                    canOutsideClickClose={true}
+                    canEscapeKeyClose={true}
+                    isOpen={this.clientToDelete !== undefined}
+                    title={this.clientToDelete !== undefined ? 'Suppression client "' + this.clientToDelete.nom + '", id[' + this.clientToDelete.id + ']' : 'Erreur client inexistant'}
+                    icon="warning-sign"
+                    onClose={() => { this.clientToDelete = undefined; }}
+                >
+                    <div className={style(csstips.flex, csstips.vertical)}>
+                        <div className={style(csstips.margin(20), csstips.flex)}>
+                            Opération irréversible : confirmer ?
+                        </div>
+                        <div className={style(csstips.margin(20), csstips.flex)}>
+                            <InputGroup
+                                placeholder="password"
+                                onChange={(event: any) => { this.password = event.target.value }}
+                                type="password"
+                            />
+                        </div>
+                        <div className={style(csstips.horizontal, csstips.flex)}>
+                            <Button
+                                className={style(csstips.margin(10), csstips.flex)}
+                                intent={Intent.NONE}
+                                text="Annuler"
+                                onClick={() => { this.clientToDelete = undefined; }}
+                            />
+                            <Button
+                                className={style(csstips.margin(10), csstips.flex)}
+                                intent={Intent.DANGER}
+                                icon="warning-sign"
+                                text="Supprimer"
+                                onClick={this.handleDeleteClient}
+                            />
+                        </div>
+                    </div>
+                </Dialog>
+                <Dialog
+                    autoFocus={true}
+                    enforceFocus={true}
+                    usePortal={true}
+                    canOutsideClickClose={true}
+                    canEscapeKeyClose={true}
+                    isOpen={this.dialogCreateClientOpened}
+                    title="Nouveau client"
+                    icon="new-person"
+                    onClose={() => { this.dialogCreateClientOpened = false; }}
+                >
+                    <div className={style(csstips.flex, csstips.vertical)}>
+                        <div className={dialogLineStyle}>
+                            <div className={dialogFieldNameStyle}>
+                                Nom
                             </div>
-                            <div className={style(csstips.horizontal, csstips.flex)}>
-                                <Button className={style(csstips.margin(10), csstips.flex)} intent={Intent.DANGER} icon="warning-sign" text="OK" onClick={this.handleDeleteClient}/>
-                                <Button className={style(csstips.margin(10), csstips.flex)} intent={Intent.NONE} text="Cancel" onClick={() => { this.clientToDelete = undefined; }}/>
+                            <div className={dialogFieldValueStyle}>
+                                <InputGroup
+                                    leftIcon="person"
+                                    placeholder="Nom Prénom"
+                                    onChange={(event: any) => { this.clientToCreate.nom = event.target.value }}
+                                />
                             </div>
                         </div>
-                    </Dialog> : ''
-                }
+                        <div className={dialogLineStyle}>
+                            <div className={dialogFieldNameStyle}>
+                                Adresse
+                            </div>
+                            <div className={dialogFieldValueStyle}>
+                                <InputGroup
+                                    leftIcon="home"
+                                    placeholder="Adresse"
+                                    onChange={(event: any) => { this.clientToCreate.adresse = event.target.value }}
+                                />
+                            </div>
+                        </div>
+                        <div className={dialogLineStyle}>
+                            <div className={dialogFieldNameStyle}>
+                                Téléphone
+                            </div>
+                            <div className={dialogFieldValueStyle}>
+                                <InputGroup
+                                    leftIcon="phone"
+                                    placeholder="Numéro de téléphone"
+                                    onChange={(event: any) => { this.clientToCreate.telephone = event.target.value }}
+                                />
+                            </div>
+                        </div>
+                        <div className={dialogLineStyle}>
+                            <div className={dialogFieldNameStyle}>
+                                e-mail
+                            </div>
+                            <div className={dialogFieldValueStyle}>
+                                <InputGroup
+                                    leftIcon="envelope"
+                                    placeholder="e-mail"
+                                    onChange={(event: any) => { this.clientToCreate.email = event.target.value }}
+                                />
+                            </div>
+                        </div>
+                        <div className={dialogLineStyle}>
+                            <div className={dialogFieldNameStyle}/>
+                            <div className={dialogFieldValueStyle}>
+                                <InputGroup
+                                    placeholder="password"
+                                    onChange={(event: any) => { this.password = event.target.value }}
+                                    type="password"
+                                />
+                            </div>
+                        </div>
+                        <div className={style(csstips.horizontal, csstips.flex)}>
+                            <Button
+                                className={style(csstips.margin(10), csstips.flex)}
+                                intent={Intent.NONE}
+                                text="Annuler"
+                                onClick={() => { this.dialogCreateClientOpened = false; }}
+                            />
+                            <Button
+                                className={style(csstips.margin(10), csstips.flex)}
+                                intent={Intent.PRIMARY}
+                                icon="add"
+                                text="Créer"
+                                onClick={this.handleCreateClient}
+                            />
+                        </div>
+                    </div>
+                </Dialog>
             </div>
         );
     }
     // http://testbase.ideesalter.com/alia_writeClient.php?id=8&nom=mon%20nom12&adresse=69%20rue%20du%20quai2&email=monNom@rueduquai2.fr&telephone=+3369696970
     // http://testbase.ideesalter.com/alia_deleteClient.php?id=8
     private handleDeleteClient = () => {
-        fetch(`http://testbase.ideesalter.com/alia_deleteClient.php?id=` + this.clientToDelete.id)
+        fetch(`http://testbase.ideesalter.com/alia_deleteClient.php?id=` + this.clientToDelete.id + `&password=` + encodeURIComponent(this.password))
             .then((response) => {
                 if (response.status === 200) {
                     this.getClients();
@@ -172,13 +284,19 @@ interface IProps {
         this.clientToDelete = undefined;
     }
 
+    private handleCreateClient = () => {
+        this.handleWriteClient(this.clientToCreate);
+        this.dialogCreateClientOpened = false;
+    };
+
     private handleWriteClient = (client: IClient) => {
         fetch(`http://testbase.ideesalter.com/alia_writeClient.php` +
             `?id=` + client.id +
             `&nom=` + client.nom + 
             `&adresse=` + client.adresse + 
             `&telephone=` + client.telephone + 
-            `&email=` + client.email
+            `&email=` + client.email +
+            `&password=` + encodeURIComponent(this.password)
         ).then((response) => {
                 if (response.status === 200) {
                     this.getClients();
